c95d21369a01e695e03f040e3807d78e7e449162
dankaufmann <info@dankaufmann.com>
2020-04-09 11:52:06 +0200

    S01E02

--- S01E01_PrimeDeRisque.R
+++ S01E01_PrimeDeRisque.R
@@ -1,5 +1,5 @@
 # ------------------------------------------------------------------------
-# R Code "La courbe de fièvre de l'économie Suisse"
+# R Code [S1E01] "La courbe de fiÃ¨vre de l'Ã©conomie Suisse"
 # ------------------------------------------------------------------------
 # Video available on www.youtube.com/channel/UCJpACBsnn1eQTObWz5LniGg
 #
@@ -21,15 +21,15 @@ startDate <- "2007-01-01"
 #-------------------------------------------------------------------------------------
 # Compute risk premia
 #-------------------------------------------------------------------------------------
-download.file(url = "https://www.bfs.admin.ch/bfsstatic/dam/assets/7966853/master", destfile = "./PrimeDeRisque/Defaults_OFS.xlsx", mode="wb")
-download.file(url = "https://www.six-group.com/exchanges/downloads/indexdata/hsb_maturity_gov_y.csv", destfile = "./PrimeDeRisque/ObligationsConf.csv", mode="wb")
-download.file(url = "https://www.six-group.com/exchanges/downloads/indexdata/hsb_maturity_dom_non_gov_rating_sbi_y.csv", destfile = "./PrimeDeRisque/ObligationsEnt.csv", mode="wb")
+download.file(url = "https://www.bfs.admin.ch/bfsstatic/dam/assets/7966853/master", destfile = "./S01E02_PrimeDeRisque/Defaults_OFS.xlsx", mode="wb")
+download.file(url = "https://www.six-group.com/exchanges/downloads/indexdata/hsb_maturity_gov_y.csv", destfile = "./S01E02_PrimeDeRisque/ObligationsConf.csv", mode="wb")
+download.file(url = "https://www.six-group.com/exchanges/downloads/indexdata/hsb_maturity_dom_non_gov_rating_sbi_y.csv", destfile = "./S01E02_PrimeDeRisque/ObligationsEnt.csv", mode="wb")
 
-Gov      <- read.csv("./PrimeDeRisque/ObligationsConf.csv", sep = ";", skip = 4)
-NonGov   <- read.csv("./PrimeDeRisque/ObligationsEnt.csv", sep = ";", skip = 4)
-Defaults <- read.xlsx("./PrimeDeRisque/Defaults_OFS.xlsx", sheetName = "T6.2.3.1", as.data.frame = TRUE)
+Gov      <- read.csv("./S01E02_PrimeDeRisque/ObligationsConf.csv", sep = ";", skip = 4)
+NonGov   <- read.csv("./S01E02_PrimeDeRisque/ObligationsEnt.csv", sep = ";", skip = 4)
+Defaults <- read.xlsx("./S01E02_PrimeDeRisque/Defaults_OFS.xlsx", sheetName = "T6.2.3.1", as.data.frame = TRUE)
 
-# Beginning of default procedures (Ouverture des procédures de faillite)
+# Beginning of default procedures (Ouverture des proc?dures de faillite)
 # I calculate the growth rate of default procedures as a share of active firms in 2017
 # https://www.bfs.admin.ch/bfs/de/home/statistiken/industrie-dienstleistungen/unternehmen-beschaeftigte/unternehmensdemografie/bestand-aktiver.html
 myDate      <- as.Date(paste(as.numeric(as.matrix((Defaults[1,2:length(Defaults[1,])]))), "-01-01", sep = ""))
@@ -63,7 +63,7 @@ AA10 <- xts(as.numeric(NonGov[, 19]), order.by = myDate2)
 # x:  risk premium
 # x = (1+i+x) - (1+i) -> We can calculate the risk premium as the rate of return between
 #                        a riskless investment and the corporate bond yield
-RPBB1  <- BBB1 - Gov1
+RPBBB1 <- BBB1 - Gov1
 RPAAA1 <- AA1 - Gov1
 
 #-------------------------------------------------------------------------------------
@@ -72,10 +72,10 @@ RPAAA1 <- AA1 - Gov1
 myLines <- c(as.numeric(as.Date("2008-09-15")), as.numeric(as.Date("2015-01-15")), as.numeric(as.Date("2020-02-01")))
 
 p <- ts_ggplot(
-  `Confédération`                     = ts_span(Gov1, startDate),
-  `Entreprises (notation AA-AAA)`    = ts_span(AA1, startDate),
-  `Entreprises (notation BBB-AAA)`    = ts_span(BBB1, startDate),
-  title = "Rendements des obligations à 1 ans (en %)"
+  `ConfÃ©dÃ©ration`                   = ts_span(Gov1, startDate),
+  `Entreprises (notation AA-AAA)`   = ts_span(AA1, startDate),
+  `Entreprises (notation BBB-AAA)`  = ts_span(BBB1, startDate),
+  title = "Rendements des obligations Ã  1 ans (en %)"
 )
 p <- p + theme_minimal() + ylab("")+xlab("")+
   ggplot2::geom_line(aes(),size=1)+ggplot2::scale_color_brewer(palette = "Dark2")+
@@ -85,12 +85,12 @@ p <- p + theme_minimal() + ylab("")+xlab("")+
   theme(panel.border = element_rect(linetype = "solid", colour = "black", fill = NA))+theme(text = element_text(family = "Palatino"))+
   theme(panel.grid.major = element_line(colour = "black",size=0.1,linetype="dotted"), panel.grid.minor = element_blank()) 
 p
-ggsave(filename = "./PrimeDeRisque/Obligations.png", width = 5, height = 4)
+ggsave(filename = "./S01E02_PrimeDeRisque/Obligations.png", width = 5, height = 4)
 
 p <- ts_ggplot(
   `Notation AA-AAA` = ts_span(RPAAA1, startDate),
   `Notation BBB-AAA` = ts_span(RPBBB1, startDate),
-  title = "Prime de risque à 1 ans (en pp)"
+  title = "Prime de risque Ã  1 ans (en pp)"
 )
 p <- p + theme_minimal() + ylab("")+xlab("")+
   ggplot2::geom_line(aes(),size=1)+ggplot2::scale_color_brewer(palette = "Dark2")+
@@ -103,12 +103,12 @@ p <- p+  geom_text(x =myLines[1], y = 1.3, label="Faillite Lehman", colour="blue
 p <- p+  geom_text(x =myLines[2], y = 1, label="Fin du taux plancher", colour="blue", angle=90, vjust = -1)
 p <- p+  geom_text(x =myLines[3], y = 1, label="Crise Corona", colour="blue", angle=90, vjust = -1)
 p
-ggsave(filename = "./PrimeDeRisque/PrimesDeRisque.png", width = 5, height = 4)
+ggsave(filename = "./S01E02_PrimeDeRisque/PrimesDeRisque.png", width = 5, height = 4)
 
 p <- ts_ggplot(
   `Prime de risque (notation BBB-AAA, en pp)`           = ts_span(RPBBB1, startDate),
   `Ouverture proc. de faillite (variation, en 1000 ouvertures par an)`  = ts_span(Faillite, startDate),
-  title = "Relation avec des procédures de faillite"
+  title = "Relation avec des procÃ©dures de faillite"
 )
 p <- p + theme_minimal() + ylab("")+xlab("")+
   ggplot2::geom_line(aes(),size=1)+ggplot2::scale_color_brewer(palette = "Dark2")+
@@ -118,5 +118,5 @@ p <- p + theme_minimal() + ylab("")+xlab("")+
   theme(panel.border = element_rect(linetype = "solid", colour = "black", fill = NA))+theme(text = element_text(family = "Palatino"))+
   theme(panel.grid.major = element_line(colour = "black",size=0.1,linetype="dotted"), panel.grid.minor = element_blank())
 p
-ggsave(filename = "./PrimeDeRisque/ProcFaillitePrime.png", width = 5, height = 4)
+ggsave(filename = "./S01E02_PrimeDeRisque/ProcFaillitePrime.png", width = 5, height = 4)
 
---
+++ readme.txt
@@ -0,0 +1,22 @@
+-----------------------------------------------------------------------------------------------
+Daniel Kaufmann, émissions "Macro-éCHo", Université de Neuchâtel
+
+daniel.kaufmann@unine.ch
+
+readme.txt for replication files
+-----------------------------------------------------------------------------------------------
+
+Notes
+-----------------------------------------------------------------------------------------------
+These files replicate the charts used in my video series "Macro-éCHo". All data sources can be 
+inferred from the codes. All files can be freely downloaded and distributed. Use them at
+your own risk with no guarantee or maintenance. If you spot an error, thanks for letting me know.
+
+The videos are available on www.youtube.com/channel/UCJpACBsnn1eQTObWz5LniGg
+
+Episodes Season 1
+-----------------------------------------------------------------------------------------------
+S01E01: Prime de risque: "La courbe de fièvre de l'économie Suisse"
+S01E02: Baromètre conjoncturel mondial: "Les saints de glace jettent un froid sur la Suisse"
+
+
